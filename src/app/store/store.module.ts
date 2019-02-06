ac28db10e0d2d747f4da62472b9fcfc325b7da7b
Dhiraj Khapangi <dkhapangi13@outlook.com>
2019-02-06 21:59:49 +0545

    Creating a Custom Directive

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from "@angular/core";
 
 import { BrowserModule } from "@angular/platform-browser";
-import { ModelModule } from "../model/model.module";
 import { FormsModule } from "@angular/forms";
+import { ModelModule } from "../model/model.module";
 
 import { StoreComponent } from "./store.component";
+
+import { CounterDirective } from "./counter.directive";
 
 @NgModule({
     imports: [
         ModelModule, BrowserModule, FormsModule
     ],
     declarations: [
-        StoreComponent
+        StoreComponent, CounterDirective
     ],
-    exports: [StoreComponent]
+    exports: [
+        StoreComponent
+    ]
 })
 
 export class StoreModule { }
